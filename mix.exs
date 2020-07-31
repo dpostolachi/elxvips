b39ae4ae0661f18386065abfc861d46adff3928b
Dumitru Postolachi <dumitru.postolachi@daliaresearch.com>
2020-07-31 18:35:32 +0200

    0.0.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Elxvips.MixProject do
   def project do
     [
       app: :elxvips,
-      version: "0.0.5",
+      version: "0.0.6",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
