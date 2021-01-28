f0ed7d0733a7751f0e9f8f6ad5e8f89fe32482dd
Dumitru Postolachi <dumitru.postolachi@daliaresearch.com>
2021-01-28 11:34:51 +0100

    update tests

--- test/elxvips_test.exs
+++ test/elxvips_test.exs
@@ -118,27 +118,24 @@ defmodule ElxvipsTest do
   end
   test "from file, autodetect format" do
 
-    sizes = from_file( "test/input.png" )
+    format = from_file( "test/input.png" )
     |> resize( width: 100, height: 100 )
     |> to_bytes()
-    |> get_image_sizes()
+    |> get_image_format()
 
-    assert sizes == { :ok, [ 100, 100 ] }
+    assert format == { :ok, :png }
 
   end
 
   test "from png to webp" do
 
-    sizes = from_file( "test/input.png" )
+    format = from_file( "test/input.png" )
     |> resize( width: 100, height: 100 )
     |> webp()
-    |> to_file( "test/output.webp" )
-    |> get_image_sizes()
-
-    assert sizes == { :ok, [ 100, 100 ] }
-
-    assert { :ok, :webp } = from_file( "test/output.webp" )
+    |> to_bytes()
     |> get_image_format()
+
+    assert format == { :ok, :webp }
 
   end
 
