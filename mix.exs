30d4cc0cf9515306ee048cefe1ac69e55571d5dc
Dumitras <alphonzoalph@gmail.com>
2021-01-31 18:40:27 +0100

    0.0.9

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Elxvips.MixProject do
   def project do
     [
       app: :elxvips,
-      version: "0.0.8",
+      version: "0.0.9",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
