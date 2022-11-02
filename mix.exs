3372facdb7d343a0cc913f99842db6f266e09154
Dima <dima@uxcam.com>
2022-11-02 12:36:33 +0100

    Prebuild

--- mix.exs
+++ mix.exs
@@ -1,10 +1,13 @@
 defmodule Elxvips.MixProject do
   use Mix.Project
 
+  @version "0.1.2"
+  @source_url "https://github.com/dpostolachi/elxvips"
+
   def project do
     [
       app: :elxvips,
-      version: "0.1.1",
+      version: @version,
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -28,7 +31,7 @@ defmodule Elxvips.MixProject do
         ],
         licenses: [ "MIT" ],
         links: %{
-            "GitHub" => "https://github.com/dpostolachi/elxvips"
+            "GitHub" => @source_url
         },
         maintainers: [ "dpostolachi" ]
     ]
@@ -50,7 +53,8 @@ defmodule Elxvips.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:rustler, "~> 0.22.2"},
+      {:rustler_precompiled, "~> 0.4"},
+      {:rustler, ">= 0.0.0", optional: true},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
