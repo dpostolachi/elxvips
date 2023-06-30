4f130f6cd14ff866d8be1c976471a7c958295456
Dima <dima@uxcam.com>
2023-06-30 21:39:53 +0200

    Update packages + prepare for v0.1.3

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Elxvips.MixProject do
   use Mix.Project
 
-  @version "0.1.2"
+  @version "0.1.3"
   @source_url "https://github.com/dpostolachi/elxvips"
 
   def project do
@@ -53,7 +53,7 @@ defmodule Elxvips.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:rustler_precompiled, "~> 0.4"},
+      {:rustler_precompiled, "~> 0.6"},
       {:rustler, ">= 0.0.0", optional: true},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
