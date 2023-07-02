b13d0b0d0eb2e09651264eca50e145804678a424
Dima <dima@uxcam.com>
2023-07-03 00:48:42 +0200

    Debug

--- checksum-Elixir.Elxvips.Native.exs
+++ checksum-Elixir.Elxvips.Native.exs
@@ -1,3 +1,3 @@
 %{
-  "liblvips-v0.1.3-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:b7b6d4604408dc7e529a107fd5602483092b375a4b43992ba2fc714ac302a941",
+  "liblvips-v0.1.3-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:a2c70fc0eee2814be7c2c053c8b3d2ddd6878e57743a85ca2d486cabb1f0a3e9",
 }
