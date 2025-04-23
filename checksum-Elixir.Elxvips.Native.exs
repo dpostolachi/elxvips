62a1ff0f4870ba5590999e899317391a9f30c357
Dima <dima@uxcam.com>
2025-04-23 16:40:39 +0200

    Checksums + update README.md

--- checksum-Elixir.Elxvips.Native.exs
+++ checksum-Elixir.Elxvips.Native.exs
@@ -1,10 +1,10 @@
 %{
-  "liblvips-v0.1.6-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:703724cf0eba24d0d01fff995cfb401602cff8e7a5e7196023d05d0ea1b6c07f",
-  "liblvips-v0.1.6-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:fe63b64bbfafed2a36ffe135a6dacf9a585084e2c1287e51cb8c9d56b1d146f4",
-  "liblvips-v0.1.6-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:9bb41b1b5b2812536291103bde40452af204be465d462efcc0f5e0eab882ecf2",
-  "liblvips-v0.1.6-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:bec3343dac253c474df204026706fc848584fda10148eba97842224fbc0b76c4",
-  "liblvips-v0.1.6-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:7def7ffd5a6d0aeb6f6606da7e832732c38bc902659249c4be5cfeee88c01ba0",
-  "liblvips-v0.1.6-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:c70645b4f8cdcb17250dc0b69bcefca1424b999f1176aaf0f17a62d296f578b7",
-  "liblvips-v0.1.6-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:cf9181d698e1fae3fb583968c6205f892d6ca108c08fb10f394166546771ac1a",
-  "liblvips-v0.1.6-nif-2.16-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:79d1ea0ceff31981147fc0e4e45d0579887b842c7a36d4159717231acb537399",
+  "liblvips-v0.1.7-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:bc7ce7428733d3f5712cd91d6498849278a733518e1203890b5009d578509fb7",
+  "liblvips-v0.1.7-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:ba1ce286c7fc750fa4b66e00c6b9565ada956e65787ca9088454e42a55e0a72d",
+  "liblvips-v0.1.7-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:d8bc08102a271512c06244f456f40cece5af8285dee1e987b1c9711bfbad06a8",
+  "liblvips-v0.1.7-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:423a5a610204dfb6a07150027c9b086bb960e7fefbf237a17e313215800617c6",
+  "liblvips-v0.1.7-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:6214587e12539a9d4e7ba6a945dd4bdbde9e1d843995b96b126828dbf9cfe77f",
+  "liblvips-v0.1.7-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:ecd834f59a5afa63849afbc8d0bb0f5cd8f4c91eee4ab7c5bccb199a48438b79",
+  "liblvips-v0.1.7-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:fd8966b4b3e9c5611c43aa6e29bd072bf42995a98708c3a54c922a2efaf01b32",
+  "liblvips-v0.1.7-nif-2.16-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:c663d959ebb14ced29c810483eebe2b4880af21c18c32581379f0cd61539a987",
 }
